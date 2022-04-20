10b5268db287cf87d85d28ec885aec29b69379e6
Lemonka93 <lerayepishko@gmail.com>
2022-04-21 03:08:32 +0400

    end: in-component route guards

--- src/views/event/Edit.vue
+++ src/views/event/Edit.vue
@@ -1,20 +1,27 @@
 <template >
-
-  <p >Edit an Event</p >
-
+  <p>Edit an Event</p>
 </template >
 
 <script >
-
-
 export default {
-  name: 'EventEdit',
-  props: ['event'],
-
-
-}
+  name: "EventEdit",
+  props: ["event"],
+  data: function () {
+    return {
+      unsavedChanges: true,
+    };
+  },
+  beforeRouteLeave() {
+    console.log("abc");
+    if (this.unsavedChanges) {
+      const answer = confirm(
+        "Do you really want to leave? You have unsaved changes!"
+      );
+      return answer;
+    }
+  },
+};
 </script >
 
 <style scoped >
-
 </style >
